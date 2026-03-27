718ccbbd681d621a15bd60e55ab17b0f3d9511d9
Sheinrei <l.beaute@laposte.net>
2026-03-27 20:04:17 +0100

    Début du react router

---
+++ front/src/page/Inscription.tsx
@@ -0,0 +1,16 @@
+
+
+export function Inscription(){
+
+    return(
+
+        <div>
+            <h1>Inscription</h1>
+            <form action="">
+
+
+
+            </form>
+        </div>
+    )
+}
--- front/src/router.tsx
+++ front/src/router.tsx
@@ -1,21 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import ContractAnalysis from "./page/ContractAnalysis"
-
-
+import { Dashboard } from "./page/Dashboard";
+import { Inscription } from "./page/Inscription";
 function Teste(){
     return (<div>Test</div>)
 }
 
-function Dashboard(){
-    return (<div>Dashboard</div>)
-}
 
 export function App() {
   return (
     <Routes>
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/about" element={<Teste />} />
-      <Route path="/" element={<ContractAnalysis />} />
+      <Route path="/teste" element={<Teste />} />
+      <Route path="/analyzer" element={<ContractAnalysis />} />
+      <Route path="/inscription" element={<Inscription />} />
     </Routes>
   );
 }
